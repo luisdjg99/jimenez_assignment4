cf57b59fda16a82f7eff3d755dba9909d2924140
Luis D Jimenez <luis_daniel99@yahoo.com>
2020-03-29 22:32:14 -0400

    Initial commit

---
+++ jimenez_problem2/src/jimenez_problem2/DuplicateCounter.java
@@ -0,0 +1,44 @@
+package jimenez_problem2;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class DuplicateCounter {
+	
+	HashMap<String, Integer> wordCounter = new HashMap<>();
+
+	   public void count(String dataFile) throws FileNotFoundException {
+
+	       File file = new File(dataFile);
+	       Scanner reader = new Scanner(file);
+
+	       while (reader.hasNext()) {
+	           String word = reader.next();
+	           if (word != null && !word.isEmpty()) {
+	               word = word.toLowerCase();
+	               if (wordCounter.containsKey(word)) {
+	                   wordCounter.put(word, wordCounter.get(word) + 1);
+	               } else {
+	                   wordCounter.put(word, 1);
+	               }
+	           }
+	       }
+	       reader.close();
+	       System.out.println(wordCounter);
+	   }
+
+	   public void write(String outputFile) throws IOException {
+
+	       FileWriter writer = new FileWriter(outputFile);
+	       for (Map.Entry<String, Integer> entry : wordCounter.entrySet()) {
+	           writer.write(entry.getKey() + "\t\t\t\t\t" + entry.getValue());
+	           writer.write("\n");
+	       }
+	       writer.close();
+	   }
+
+}
